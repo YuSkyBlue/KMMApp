3f85f169268bb0e080dac50223ea69bc340a6ab5
ck <dbcjdrb1997@gmail.com>
2023-11-19 23:18:15 +0900

    5.3-VoiceToTextViewModel

---
+++ androidApp/src/main/java/com/bluesky/kmmapp/android/voice_to_text/presentation/AndroidVoiceToTextParser.kt
@@ -0,0 +1,28 @@
+package com.bluesky.kmmapp.android.voice_to_text.presentation
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bluesky.kmmapp.voice_to_text.domain.VoiceToTextParser
+import com.bluesky.kmmapp.voice_to_text.presentation.VoiceToTextEvent
+import com.bluesky.kmmapp.voice_to_text.presentation.VoiceToTextViewModel
+
+@HiltViewModel
+class AndroidVoiceToTextParser @Inject constructor(
+    private val parser: VoiceToTextParser
+): ViewModel() {
+
+    private val viewModel by lazy {
+        VoiceToTextViewModel(
+            parser = parser,
+            coroutineScope = viewModelScope
+        )
+    }
+
+    val state = viewModel.state
+
+    fun onEvent(event: VoiceToTextEvent) {
+        viewModel.onEvent(event)
+    }
+}
